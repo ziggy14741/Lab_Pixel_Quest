d5505357fe0f7ecd94bc93be37290fe08e9cd21a
Sebastian Grygorczuk <stem08992@gmail.com>
2024-10-21 11:32:58 -0400

    Class Udpate

--- Assets/Scripts/PlayerJumping.cs
+++ Assets/Scripts/PlayerJumping.cs
@@ -20,14 +20,19 @@ public class PlayerJumping : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Checks if the player is touching any object who's layermask is called Ground
         bool groundCheck = Physics2D.OverlapCapsule(ground.position, new Vector2(1, 0.08f), CapsuleDirection2D.Horizontal, 0, groundMask);
 
+
+        // Checks if the player can jump
+        // Checks if the player is click the space key, checks if the player is touching ground or water
         if (Input.GetKeyDown(KeyCode.Space) && (groundCheck || waterCheck ))
         {
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpForce);
         }
     }
 
+    // Checks if the player has touched water, if so let them jump 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Water")

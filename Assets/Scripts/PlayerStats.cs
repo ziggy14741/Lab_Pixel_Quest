9c20675d886cde7fe5dfea615c4ff1190c39653f
Sebastian Grygorczuk <stem08992@gmail.com>
2024-10-23 18:23:36 -0400

    10.23

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
     public Transform respawnPoint;
     public int playerLife = 3;
+    public int currentCoins = 0;
 
     private Rigidbody2D rigidbody2D; 
 
     private const string deathTag = "Death";
+    private const string healthTag = "Health";
+    private const string coinTag = "Coin";
+    private const string respawnTag = "Respawn";
+    private const string finishTag = "Finish";
 
     void Start()
     {
@@ -24,6 +30,33 @@ public class PlayerStats : MonoBehaviour
                     rigidbody2D.velocity = Vector2.zero;
                     transform.position = respawnPoint.position;
                     playerLife--;
+                    if (playerLife <= 0)
+                    {
+                        string currentLevel = SceneManager.GetActiveScene().name;
+                        SceneManager.LoadScene(currentLevel);
+                    }
+                    break;
+                }
+            case healthTag:
+                {
+                    playerLife++;
+                    Destroy(collision.gameObject);
+                    break;
+            }
+            case coinTag:
+                {
+                    currentCoins++;
+                    Destroy(collision.gameObject);
+                    break;
+                }
+            case respawnTag:
+                {
+                    respawnPoint.position = collision.transform.position;
+                    break;
+                }
+            case finishTag:
+                {
+                    SceneManager.LoadScene("Level_2");
                     break;
                 }
         }

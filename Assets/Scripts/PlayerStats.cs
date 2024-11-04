56ca780f3c4be9e21d7586f7b6cd7f07084d833d
Sebastian Grygorczuk <stem08992@gmail.com>
2024-11-04 17:27:36 -0500

    Part 1

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerStats : MonoBehaviour
 {
     public Transform respawnPoint;
-    public int playerLife = 3;
+    public float playerLife = 3;
     public int currentCoins = 0;
 
     private Rigidbody2D rigidbody2D; 
@@ -17,19 +19,36 @@ public class PlayerStats : MonoBehaviour
     private const string respawnTag = "Respawn";
     private const string finishTag = "Finish";
 
+    public Image heartImage;
+    private float playerMaxHealth = 3;
+
+    public TextMeshProUGUI coinText;
+    public GameObject CoinParent;
+    private int maxCoins; 
+
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        maxCoins = CoinParent.transform.childCount;
+        coinText.text = currentCoins + "/" + maxCoins;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switch(collision.tag){
+            case coinTag:
+                {
+                    currentCoins++;
+                    coinText.text = currentCoins + "/" + maxCoins;
+                    Destroy(collision.gameObject);
+                    break;
+                }
             case deathTag:
                 {
                     rigidbody2D.velocity = Vector2.zero;
                     transform.position = respawnPoint.position;
                     playerLife--;
+                    heartImage.fillAmount = playerLife / playerMaxHealth;
                     if (playerLife <= 0)
                     {
                         string currentLevel = SceneManager.GetActiveScene().name;
@@ -39,16 +58,14 @@ public class PlayerStats : MonoBehaviour
                 }
             case healthTag:
                 {
-                    playerLife++;
-                    Destroy(collision.gameObject);
+                    if(playerLife < playerMaxHealth)
+                    {
+                        playerLife++;
+                        heartImage.fillAmount = playerLife / playerMaxHealth;
+                        Destroy(collision.gameObject);
+                    }
                     break;
             }
-            case coinTag:
-                {
-                    currentCoins++;
-                    Destroy(collision.gameObject);
-                    break;
-                }
             case respawnTag:
                 {
                     respawnPoint.position = collision.transform.Find("RespawnPoint").position;
